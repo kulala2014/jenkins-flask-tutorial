924aac71df520056ff1c6780b3c94925e755a86b
kulala2014 <125820124@qq.com>
2020-10-10 10:04:05 +0800

    TEST Jenkines buind

--- flask_docker.py
+++ flask_docker.py
@@ -6,8 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask Dockerized'
 
-def main():
-    app.run(debug=True, host='0.0.0.0')
-
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, host='0.0.0.0')
